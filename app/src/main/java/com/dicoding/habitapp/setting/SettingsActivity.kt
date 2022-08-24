e462eb0e07473ff9a113dd6adb6f2cd2f8372f94
Muhammad Rafiadly <muhammadrafiadly@gmail.com>
2022-08-25 06:55:46 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/habitapp/setting/SettingsActivity.kt
@@ -0,0 +1,51 @@
+package com.dicoding.habitapp.setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.PreferenceFragmentCompat
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.utils.DarkMode
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            //TODO 11 : Update theme based on value in ListPreference
+            val switchDarkMode: ListPreference? = findPreference(getString(R.string.pref_key_dark))
+            switchDarkMode?.setOnPreferenceChangeListener{ preference, newValue ->
+                val stringValue = newValue.toString()
+                if(stringValue == getString(R.string.pref_dark_auto)){
+                    if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) updateTheme(
+                        DarkMode.FOLLOW_SYSTEM.value)
+                    else updateTheme(DarkMode.OFF.value)
+                }
+                else if(stringValue == getString(R.string.pref_dark_off)) updateTheme(DarkMode.OFF.value)
+                else updateTheme(DarkMode.ON.value)
+
+                true
+            }
+        }
+
+        private fun updateTheme(mode: Int): Boolean {
+            AppCompatDelegate.setDefaultNightMode(mode)
+            requireActivity().recreate()
+            return true
+        }
+    }
+}
